5728025b241d90a0716ce285ab2f6e15908fb1dd
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-03-08 21:41:58 +0200

    aula04

---
+++ ex04.pas
@@ -0,0 +1,24 @@
+program calcular;
+
+USES
+    SysUtils;
+var 
+    a, b, c, d: Integer;
+
+begin
+
+    a:= 17;
+    b:= 4;
+
+     
+
+    writeln('---------------------------------------');
+    
+    writeln((a>b) and (c<b));
+    writeln(not(a<>b) or (c<b)); 
+     
+     
+
+
+
+End.
