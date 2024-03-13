7db606c54a5afde4ac53b3baed4a77452f2ee1a2
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-03-13 14:09:45 +0200

    Update tempCodeRunnerFile.pas

--- tempCodeRunnerFile.pas
+++ tempCodeRunnerFile.pas
@@ -1,1 +1,28 @@
-Delta
+
+program  d;
+
+
+var 
+    a, b, soma: real;
+
+begin 
+
+    writeln('numeros: ');
+    readln(a);
+
+    if a<=4 then
+    begin 
+        writeln('Reprovado');
+    end
+
+    begin 
+    else if a>=5 and a<=10 then 
+        writeln('erecuperção...!');
+    end 
+
+    begin
+    else
+        writeln('Aprovado');
+    end
+
+End.
