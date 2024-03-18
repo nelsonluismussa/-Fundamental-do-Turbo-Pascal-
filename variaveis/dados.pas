16b3af5b32c600b621e532c9a742ca172655d9bd
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-03-18 09:51:16 +0200

    aula

--- variaveis/dados.pas
+++ variaveis/dados.pas
@@ -1,20 +1,39 @@
-Program Operadores;
-
-uses
-        crt;
+program TiposDeDados;
 
 var
-        a, b, soma, diferenca, produto, quociente, resto: Integer;
+  // Variáveis inteiras
+  idade: Integer;
+  quantidade: LongInt; // Para números inteiros maiores
 
-Begin
-        clrscr;
+  // Variáveis de ponto flutuante
+  altura: Real; // Pode representar números reais com precisão limitada
+  peso: Double; // Para números reais com maior precisão
 
-        writeln('Digite o primeiro valor:');
-        readln(a);
+  // Variável booleana
+  ehAdulto: Boolean;
 
-        writeln('Digite o segundo valor:');
-        readln(b);
+  // Variável de caractere
+  inicial: Char;
 
+  // Variável de cadeia de caracteres (string)
+  nome: String;
 
+begin
+  // Atribuindo valores às variáveis
+  idade := 25;
+  quantidade := 1000000000;
+  altura := 1.75;
+  peso := 68.5;
+  ehAdulto := True;
+  inicial := 'J';
+  nome := 'João';
 
-End.
+  // Exibindo os valores das variáveis
+  writeln('Idade:', idade);
+  writeln('Quantidade:', quantidade);
+  writeln('Altura:', altura:0:2); // Exibe a altura com 2 casas decimais
+  writeln('Peso:', peso:0:1); // Exibe o peso com 1 casa decimal
+  writeln('É adulto?', ehAdulto);
+  writeln('Inicial:', inicial);
+  writeln('Nome:', nome);
+end.
