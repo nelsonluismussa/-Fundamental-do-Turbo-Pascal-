74cef5bdc5b7c23d5a8109a4f9ef4423e2264bd8
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-03-18 09:35:11 +0200

    aula

--- operadores/Aritmeticos/aula01.pas
+++ operadores/Aritmeticos/aula01.pas
@@ -8,3 +8,24 @@
     Módulo (resto da divisão): mod
 }
 
+Program Operadores;
+
+var
+  a, b, soma, diferenca, produto, quociente, resto: Integer;
+
+begin
+  a := 10;
+  b := 3;
+  soma := a + b;
+  diferenca := a - b;
+  produto := a * b;
+  quociente := a div b;
+  resto := a mod b;
+
+  WriteLn('Soma:', soma);
+  WriteLn('Diferenca:', diferenca);
+  WriteLn('Produto:', produto);
+  WriteLn('Quociente:', quociente);
+  WriteLn('Resto:', resto);
+
+end.
