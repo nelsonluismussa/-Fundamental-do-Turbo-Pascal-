1b23460f9f4509cdf956bd3122048cda83e0be7a
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-03-18 09:22:30 +0200

    aula

---
+++ variaveis/dados.pas
@@ -0,0 +1,20 @@
+Program Operadores;
+
+uses
+        crt;
+
+var
+        a, b, soma, diferenca, produto, quociente, resto: Integer;
+
+Begin
+        clrscr;
+
+        writeln('Digite o primeiro valor:');
+        readln(a);
+
+        writeln('Digite o segundo valor:');
+        readln(b);
+
+
+
+End.
