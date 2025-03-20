8c8144c883cff734476061889fab22ef0ed1e74a
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-20 18:50:36 +0200

    aula01

---
+++ variaveis/ex01.pas
@@ -0,0 +1,31 @@
+Program Calculadora;
+
+var
+   // Variaveis
+	a: integer;
+	b: integer;
+	soma : real;
+	subtracao: real;
+	divisao1: real;
+	restodaDivisao: integer;
+	
+	 
+begin
+   
+	writeln('infome os valores de a e b');  // imprimi
+	readln(a, b);                           // lê os valor a e b
+	
+  
+	soma := a + b;
+	subtracao := a -  b;
+	divisao1 := a / b;
+	restodaDivisao := a mod b;
+	 
+	
+	writeln('a soma total:', soma:0:1);
+	writeln('a Subtracao total:', subtracao:0:1);
+	writeln('a divisoa1:', divisao1:0:1);
+	writeln('a restodaDivisao:', restodaDivisao);
+	
+     readln;
+End.
